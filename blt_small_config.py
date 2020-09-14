be76eec5d116c8bc72b456da5a3c13eb545fad9b
Ben Glick <glick@glick.cloud>
2020-09-14 11:42:55 -0700

    Update and rename funcx_config.py to blt_small_config.py

--- blt_small_config.py
+++ blt_small_config.py
@@ -4,18 +4,18 @@ from parsl.providers import GridEngineProvider
 from parsl.addresses import address_by_route
 
 config = Config(provider=GridEngineProvider(walltime='10000:00:00',
-                                                                  nodes_per_block=1,
-                                                                  init_blocks=1,
-                                                                  max_blocks=4,
-                                                                  scheduler_options="#$ -pe smp 2"
-                                                                  )
+                                            nodes_per_block=1,
+                                            init_blocks=1,
+                                            max_blocks=4,
+                                            scheduler_options="#$ -pe smp 1"
+                                            )
 )
 
 # For now, visible_to must be a list of URNs for globus auth users or groups, e.g.:
 # urn:globus:auth:identity:{user_uuid}
 # urn:globus:groups:id:{group_uuid}
 meta = {
-    "name": "blt_endpoinnt",
+    "name": "blt_small",
     "description": "",
     "organization": "",
     "department": "",
