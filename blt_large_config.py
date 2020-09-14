47eb846dc1c60111c9a60ee48a2fbce2e5a43cad
Ben Glick <glick@glick.cloud>
2020-09-14 11:50:52 -0700

    Create blt_large_config.py

---
+++ blt_large_config.py
@@ -0,0 +1,23 @@
+from funcx.config import Config
+from parsl.providers import GridEngineProvider
+from parsl.addresses import address_by_route
+
+config = Config(provider=GridEngineProvider(walltime='10000:00:00',
+                                            nodes_per_block=1,
+                                            init_blocks=1,
+                                            max_blocks=4,
+                                            scheduler_options="#$ -pe smp 8"),
+                 cores_per_worker=8,
+                 )
+
+# For now, visible_to must be a list of URNs for globus auth users or groups, e.g.:
+# urn:globus:auth:identity:{user_uuid}
+# urn:globus:groups:id:{group_uuid}
+meta = {
+    "name": "blt_large",
+    "description": "",
+    "organization": "",
+    "department": "",
+    "public": False,
+    "visible_to": []
+}
