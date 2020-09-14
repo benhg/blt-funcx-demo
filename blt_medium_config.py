4f0db795bfaf7ce1b94db3604aecc2e0ad7d92c1
Ben Glick <glick@glick.cloud>
2020-09-14 11:48:50 -0700

    Create blt_medium_config.py

---
+++ blt_medium_config.py
@@ -0,0 +1,24 @@
+
+from funcx.config import Config
+from parsl.providers import GridEngineProvider
+from parsl.addresses import address_by_route
+
+config = Config(provider=GridEngineProvider(walltime='10000:00:00',
+                                            nodes_per_block=1,
+                                            init_blocks=1,
+                                            max_blocks=4,
+                                            scheduler_options="#$ -pe smp 4"),
+                 cores_per_worker=4,
+                 )
+
+# For now, visible_to must be a list of URNs for globus auth users or groups, e.g.:
+# urn:globus:auth:identity:{user_uuid}
+# urn:globus:groups:id:{group_uuid}
+meta = {
+    "name": "blt_medium",
+    "description": "",
+    "organization": "",
+    "department": "",
+    "public": False,
+    "visible_to": []
+}
